35914a062707b383d561a65d73d2382ce9b3a117
isaacwambu <codeacademyj@gmail.com>
2021-11-15 03:48:34 +0300

    add form validation in html using required

---
+++ ip.js
@@ -0,0 +1,16 @@
+
+    function fn(){
+        var d = new Date();
+        var day = document.getElementById("day").value;
+        var month = document.getElementById("month").value;
+        var year = document.getElementById("year").value;
+
+        d = new Date(3);
+        d = new Date(3);
+        d = new Date(1990);
+
+
+        alert(d);         
+        }
+       
+       
